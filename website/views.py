bc2d6e358a68f92dd2c2723f2d468506a90213c8
JS-jopepe <jovenel.s@outlook.com>
2019-12-23 16:46:59 -0300

    instalando o django e o django-admin

--- website/views.py
+++ website/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    nome = EFA- ENGLISH FOR ALL 
+    lema = Formando um futuro melhor
+    numero = +55-11-98399-8381
+    lista_de_cursos = [
+        'Inglês',
+        'Informática básica',
+        'Francês',
+        'Reforço Preparatórios Extra Curricular', 
+    ]
+    
+    context = {
+        'nome' = nome,
+        'lema' = lema,
+        'numero' = numero,
+        'cursos' = lista_de_cursos,
+    }
+    return render(request, home.html, context)
